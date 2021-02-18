0015435d06e7ec8f04fb9350373e693cbd8556b6
Anna Wong <76626238+aw0ng@users.noreply.github.com>
2021-02-17 21:44:12 -0500

    toolbar, notecontainer, noteselectors, noteselector and noteeditor components

---
+++ src/components/NoteSelector.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="note-selector" v-bind:class="{ active: selectedNoteId === note.id }" v-on:click="selectNote(note)">
+    <p class="note-selector-title">{{ note.body }}</p>
+    <p class="note-selector-timestamp">{{ note.timestamp }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["note", "selectedNoteId"],
+  methods: {
+    selectNote: function(note) {
+      console.log("HELLO");
+      this.$emit("selectNote", note);
+      // this.selectedNoteId = note.id;
+    },
+  },
+};
+</script>
---
+++ src/components/NoteSelectors.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="note-selectors">
+    <NoteSelector
+      v-for="note in notes"
+      v-bind:key="note.id"
+      v-bind:note="note"
+      v-bind:selectedNoteId="selectedNoteId"
+      v-on:selectNote="selectNote"
+    />
+  </div>
+</template>
+
+<script>
+import NoteSelector from "./NoteSelector";
+
+export default {
+  components: {
+    NoteSelector,
+  },
+  data: function() {
+    return {
+      notes: [
+        { id: 1, body: "First test", timestamp: Date.now() },
+        { id: 2, body: "Second test", timestamp: Date.now() },
+        { id: 3, body: "Third test", timestamp: Date.now() },
+      ],
+      selectedNoteId: 1,
+    };
+  },
+  methods: {
+    selectNote: function(note) {
+      this.selectedNoteId = note.id;
+    },
+  },
+};
+</script>
